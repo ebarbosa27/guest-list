2bd79a09d62245a45bb84fd471fa3af72910a4b2
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-13 19:51:56 -0500

    added two component functions to outline the structure of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import GuestList from "./GuestList";
+import GuestDetails from "./GuestDetails";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Guest List</h1>
+      <GuestList></GuestList>
+      <GuestDetails></GuestDetails>
+    </>
+  );
 }
---
+++ src/GuestList.jsx
@@ -0,0 +1,12 @@
+export default function GuestList() {
+  return (
+    <table style={{ border: "solid 1px black" }}>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+      </tr>
+      {/* logic to loop through guest list */}
+    </table>
+  );
+}
