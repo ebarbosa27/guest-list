d1febf488fedf2b2c7f1a7468bc7b52024a16b40
ebarbosa27 <ebarbosa5449@gmail.com>
2025-07-13 20:00:26 -0500

    change in guest list component to not get syntax error for table

--- src/GuestList.jsx
+++ src/GuestList.jsx
@@ -1,12 +1,14 @@
 export default function GuestList() {
   return (
     <table style={{ border: "solid 1px black" }}>
-      <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Phone</th>
-      </tr>
-      {/* logic to loop through guest list */}
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+        </tr>
+      </thead>
+      <tbody>{/* logic to loop through guest list */}</tbody>
     </table>
   );
 }
